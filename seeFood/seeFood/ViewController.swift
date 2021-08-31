ddec2ebe6441c86f541cd203a6ce264fc79411ad
Deepak Singh <dv72276@gmail.com>
2021-09-01 05:17:49 +0530

    Added MobileNetV@ ML Model- not yet incorporated

---
+++ seeFood/seeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seeFood
+//
+//  Created by Deepak Singh on 01/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
